7804133bdc7f8e85f16dd62e8eaf0607c7feb230
faizkhan421 <khanfaizy894@gmail.com>
2025-06-03 17:39:55 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/* This is a custom config.php file for YOURLS */
+
+define( 'YOURLS_DB_USER', 'if0_38614495' );
+define( 'YOURLS_DB_PASS', 'zgVyH55lyfeZYH' );
+define( 'YOURLS_DB_NAME', 'if0_38614495_XXX' );
+define( 'YOURLS_DB_HOST', 'sql105.infinityfree.com' );
+define( 'YOURLS_DB_PREFIX', 'yourls_' );
+
+define( 'YOURLS_SITE', 'http://yourdomain.com' ); // Change this to your domain
+
+define( 'YOURLS_HOURS_OFFSET', 0 );
+
+define( 'YOURLS_LANG', '' );
+
+define( 'YOURLS_PRIVATE', true );
+
+/** Usernames and passwords for YOURLS access **/
+$yourls_user_passwords = array(
+    'admin' => 'adminpass'  // You can change this password
+);
+
+define( 'YOURLS_URL_CONVERT', 36 );
+
+define( 'YOURLS_DEBUG', false );
+
+/* End of YOURLS config.php */
